f66c17b2619e992971dab8ccabc2d8941a0c3655
Mohammed Raza <razarajwani@live.com>
2022-06-25 08:14:55 +0300

    Route Updates

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fbaa5710.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
